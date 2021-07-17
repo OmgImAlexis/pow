06eb69b125cb8ea39a28e931dd75849a390fa97c
Alexis Tyler <xo@wvvw.me>
2021-07-17 09:35:10 +0930

    init

---
+++ src/index.ts
@@ -0,0 +1,171 @@
+import uWS from 'uWebSockets.js';
+import { parse as parseQueryString } from 'querystring';
+
+interface Options {
+    host?: string;
+    port?: number;
+}
+
+export interface Request {
+    query: Record<string, unknown>;
+    headers: Record<string, unknown>;
+}
+export interface Response {
+    send(body: string | object): void;
+}
+
+export type NextFunction = () => void;
+
+export type RouteHandler = (request: Request, response: Response, next: NextFunction) => Promise<unknown> | unknown;
+
+export class App {
+    public readonly host: string;
+    #app: uWS.TemplatedApp;
+    #port: number;
+    #socket?: uWS.us_listen_socket;
+
+    constructor(options: Options) {
+        this.host = options.host === 'localhost' ? '127.0.0.1' : (options.host ?? '0.0.0.0');
+        this.#port = options.port ?? 0;
+
+        this.#app = uWS.App();
+    }
+
+    public get port() {
+        return this.#port;
+    }
+
+    public use(path: string, handler: RouteHandler) {
+        this.handle('any', path, handler);
+    }
+
+    public get(path: string, handler: RouteHandler) {
+        this.handle('get', path, handler);
+    }
+
+    public post(path: string, handler: RouteHandler) {
+        this.handle('post', path, handler);
+    }
+
+    public put(path: string, handler: RouteHandler) {
+        this.handle('put', path, handler);
+    }
+
+    public patch(path: string, handler: RouteHandler) {
+        this.handle('patch', path, handler);
+    }
+
+    public delete(path: string, handler: RouteHandler) {
+        this.handle('del', path, handler);
+    }
+
+    public options(path: string, handler: RouteHandler) {
+        this.handle('options', path, handler);
+    }
+
+    public connect(path: string, handler: RouteHandler) {
+        this.handle('connect', path, handler);
+    }
+
+    public head(path: string, handler: RouteHandler) {
+        this.handle('head', path, handler);
+    }
+    
+    public listen(done?: (options: { host: string; port: number; }) => void) {
+        this.#app.listen(this.host, this.port, socket => {
+            // Save this socket for graceful closing
+            this.#socket = socket;
+
+            // If we passed 0 then resolve the port
+            if (this.#port === 0) this.#port = uWS.us_socket_local_port(socket);
+            done?.({ host: this.host, port: this.port });
+        });
+    }
+
+    public close() {
+        if (this.#socket) uWS.us_listen_socket_close(this.#socket);
+    }
+
+    private handle(method: 'any' | 'get' | 'post' | 'put' | 'patch' | 'del' | 'options' | 'connect' | 'head', path: string, handler: RouteHandler) {
+        this.#app[method](path, async (res, req) => {
+            res.onAborted(() => {
+                res.aborted = true;
+            });
+
+            const headers: Record<string, any> = {};
+            req.forEach((key, value) => {
+                headers[key] = value;
+            });
+
+            const request = {
+                query: parseQueryString(req.getQuery()),
+                headers,
+            };
+            const response = {
+                send(body: string | object) {
+                    // Bail as the response is already aborted
+                    if (res.aborted) return;
+                    // Bail as the response has already been sent
+                    if (res.done) return;
+                    // Bail as this isn't the correct handler for this route
+                    if (res.calledNext) return;
+
+                    // Mark send as being used
+                    res.didCallSend = true;
+
+                    // Process string
+                    if (typeof body === 'string') {
+                        res.done = true;
+                        res.cork(() => {
+                            res.writeStatus('200 OK').end(body);
+                        });
+                        return;
+                    }
+
+                    // Process JSON object
+                    if (typeof body === 'object') {
+                        res.done = true;
+                        res.cork(() => {
+                            res.writeStatus('200 OK').writeHeader('Content-Type', 'application/json').end(JSON.stringify(body));
+                        });
+                    }
+                }
+            };
+            const next = () => {
+                res.calledNext = true;
+                req.setYield(true);
+            };
+            try {
+                const body = await Promise.resolve(handler.bind(handler)(request, response, next));
+
+                // Bail as the response has already been sent using res.send
+                if (res.didCallSend) return;
+
+                // Bail as the body is empty
+                if (!body) return;
+
+                // Throw as they returned an error
+                if (body instanceof Error) throw body;
+
+                // Send the response
+                response.send(body as string | JSON);
+            } catch (error) {
+                // Bail as the response is already aborted
+                if (res.aborted) return;
+                // Bail as the response has already been sent
+                if (res.done) return;
+                // Bail as this isn't the correct handler for this route
+                if (res.calledNext) return;
+
+                // Reply with error
+                res.done = true;
+                res.cork(() => {
+                    const status = parseInt(error?.status ?? '500', 10);
+                    const reason = error?.reason || error?.message || (status === 500 ? 'Internal Server Error' : '');
+                    const message = `${status}${reason ? (' ' + reason) : ''}`;
+                    res.writeStatus(message).end(message);
+                });
+            }
+        });
+    }
+};
---
+++ test/http.ts
@@ -0,0 +1,361 @@
+import ava, { TestInterface } from 'ava';
+import fetch, { FetchError } from 'node-fetch';
+import { App } from '../src';
+
+const test = ava as TestInterface<{
+    app: App
+}>;
+
+test.beforeEach(t => {
+    // Create the app
+    t.context = {
+        app: new App({
+            host: 'localhost',
+            port: 0
+        })
+    };
+});
+
+test.afterEach(t => {
+    // Stop the app
+    t.context.app.close();
+});
+
+test('basic GET request', async t => {
+    const app = t.context.app;
+
+    // Add GET route
+    app.get('/', () => {
+        return 'Hello World!';
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+    t.is(response.status, 200);
+    t.is(await response.text(), 'Hello World!');
+});
+
+test('basic POST request', async t => {
+    const app = t.context.app;
+
+    // Add POST route
+    app.post('/', () => {
+        return 'Hello World!';
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'POST' });
+    t.is(response.status, 200);
+    t.is(await response.text(), 'Hello World!');
+});
+
+test('basic DELETE request', async t => {
+    const app = t.context.app;
+
+    // Add DELETE route
+    app.delete('/', () => {
+        return 'Hello World!';
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'DELETE' });
+    t.is(response.status, 200);
+    t.is(await response.text(), 'Hello World!');
+});
+
+test('basic PATCH request', async t => {
+    const app = t.context.app;
+
+    // Add PATCH route
+    app.patch('/', () => {
+        return 'Hello World!';
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'PATCH' });
+    t.is(response.status, 200);
+    t.is(await response.text(), 'Hello World!');
+});
+
+test('basic PUT request', async t => {
+    const app = t.context.app;
+
+    // Add PUT route
+    app.put('/', () => {
+        return 'Hello World!';
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'PUT' });
+    t.is(response.status, 200);
+    t.is(await response.text(), 'Hello World!');
+});
+
+test('basic use request', async t => {
+    const app = t.context.app;
+
+    // Add use route
+    app.use('/', () => {
+        return 'Hello World!';
+    });
+
+    // Start app
+    app.listen();
+
+    // GET
+    {
+        const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+        t.is(response.status, 200);
+        t.is(await response.text(), 'Hello World!');
+    }
+
+    // POST
+    {
+        const response = await fetch(`http://localhost:${app.port}/`, { method: 'POST' });
+        t.is(response.status, 200);
+        t.is(await response.text(), 'Hello World!');
+    }
+
+    // DELETE
+    {
+        const response = await fetch(`http://localhost:${app.port}/`, { method: 'DELETE' });
+        t.is(response.status, 200);
+        t.is(await response.text(), 'Hello World!');
+    }
+
+    // HEAD
+    {
+        const response = await fetch(`http://localhost:${app.port}/`, { method: 'HEAD' });
+        t.is(response.status, 200);
+        t.is(await response.text(), '');
+    }
+
+    // OPTIONS
+    {
+        const response = await fetch(`http://localhost:${app.port}/`, { method: 'OPTIONS' });
+        t.is(response.status, 200);
+        t.is(await response.text(), 'Hello World!');
+    }
+
+    // RANDOM_HAT
+    // This should fail as it's not a valid HTTP method
+    {
+        try {
+            await fetch(`http://localhost:${app.port}/`, { method: 'RANDOM_HAT' });
+        } catch (error: unknown) {
+            t.true(error instanceof FetchError);
+            t.is((error as FetchError).code, 'ECONNRESET');
+            t.true((error as FetchError).message.endsWith('reason: socket hang up'));
+        }
+    }
+});
+
+test('headers', async t => {
+    const app = t.context.app;
+
+    // Add GET route
+    app.get('/', () => {
+        return 'Hello World!';
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+    t.is(response.status, 200);
+    t.is(await response.text(), 'Hello World!');
+    t.deepEqual(Object.fromEntries(response.headers.entries()), {
+        'content-length': '12',
+        uwebsockets: '19'
+    });
+});
+
+test('error thrown', async t => {
+    const app = t.context.app;
+
+    // Add GET route
+    app.get('/', () => {
+        throw new Error('TEST_ERROR');
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+    t.is(response.status, 500);
+    t.is(await response.text(), '500 TEST_ERROR');
+});
+
+test('error thrown with no message', async t => {
+    const app = t.context.app;
+
+    // Add GET route
+    app.get('/', () => {
+        throw new Error();
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+    t.is(response.status, 500);
+    t.is(await response.text(), '500 Internal Server Error');
+});
+
+test('error thrown with custom status code', async t => {
+    const app = t.context.app;
+
+    class CustomError extends Error {
+        public status = 401;
+    }
+
+    // Add GET route
+    app.get('/', () => {
+        throw new CustomError('TEST_ERROR');
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+    t.is(response.status, 401);
+    t.is(await response.text(), '401 TEST_ERROR');
+});
+
+test('error returned', async t => {
+    const app = t.context.app;
+
+    // Add GET route
+    app.get('/', () => {
+        return new Error('TEST_ERROR');
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+    t.is(response.status, 500);
+    t.is(await response.text(), '500 TEST_ERROR');
+});
+
+test('returning a string', async t => {
+    const app = t.context.app;
+
+    // Add GET route
+    app.get('/', () => {
+        return 'Hello World!';
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+    t.is(response.status, 200);
+    t.is(await response.text(), 'Hello World!');
+});
+
+test('returning an object', async t => {
+    const app = t.context.app;
+
+    // Add GET route
+    app.get('/', () => {
+        return {
+            test: 'Hello',
+            anotherTest: 'World!'
+        };
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+    t.is(response.status, 200);
+    t.deepEqual(await response.json(), {
+        test: 'Hello',
+        anotherTest: 'World!'
+    });
+});
+
+test('using res.send()', async t => {
+    const app = t.context.app;
+
+    // Add GET route
+    app.get('/', (_, res) => {
+        res.send('Hello World!');
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+    t.is(response.status, 200);
+    t.is(await response.text(), 'Hello World!');
+});
+
+test('using return res.send()', async t => {
+    const app = t.context.app;
+
+    // Add GET route
+    app.get('/', (_, res) => {
+        return res.send('Hello World!');
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+    t.is(response.status, 200);
+    t.is(await response.text(), 'Hello World!');
+});
+
+test('using next()', async t => {
+    const app = t.context.app;
+
+    // Add route that'll be skipped
+    app.get('/', (req, res, next) => {
+        next();
+    });
+
+    // Add GET route
+    app.get('/', () => {
+        return 'Hello World!';
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+    t.is(response.status, 200);
+    t.is(await response.text(), 'Hello World!');
+});
+
+test('returning next()', async t => {
+    const app = t.context.app;
+
+    // Add route that'll be skipped
+    app.get('/', (req, res, next) => {
+        return next();
+    });
+
+    // Add GET route
+    app.get('/', () => {
+        return 'Hello World!';
+    });
+
+    // Start app
+    app.listen();
+
+    const response = await fetch(`http://localhost:${app.port}/`, { method: 'GET' });
+    t.is(response.status, 200);
+    t.is(await response.text(), 'Hello World!');
+});
